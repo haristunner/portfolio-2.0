3a037f54edc98f460f4b5d44f164452daece3d72
haristunner <hari17prasathb@gmail.com>
2023-03-21 23:44:01 +0530

    about page updated and scroll bottom enabled

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,14 +3,24 @@ import "./Home.css";
 import profile from "../../assets/profile2.jpg";
 
 export const Home = () => {
+
+  const scrollBottom=()=>{
+    const currentPosition = window.pageYOffset;
+    const nextPosition = currentPosition + window.innerHeight;
+    window.scrollTo({
+      top: nextPosition,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <div className="Home">
       <div className="home__container">
         <div className="home__paragraph">
           <h1>Hi, My name is</h1>
           <h3>HARI PRASATH</h3>
-          <p>A React Developer</p>
-          <button>Know More</button>
+          <p>A React Developer{" < />"}</p>
+          <button onClick={scrollBottom}>Know More</button>
         </div>
         <div className="home__img">
           <img src={profile} alt="" />
